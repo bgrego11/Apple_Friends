437d2d5b2fc9d5dadb1d037caa2dc967a9d0186a
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2017-08-31 21:42:52 -0400

    fb login

--- app.js
+++ app.js
@@ -2,6 +2,47 @@ var express = require('express');
 var app = express();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);
+var passport = require('passport')
+, FacebookStrategy = require('passport-facebook').Strategy;
+app.use(require('serve-static')(__dirname + '/../../public'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+
+var passport = require('passport')
+FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+  clientID: '1531158273642023',
+  clientSecret: '93aef4aa5842c1247b066c6c888a3116',
+  callbackURL: "http://localhost:3000/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  if (profile) {
+    user = profile;
+    return done(null, user);
+    }
+    else {
+    return done(null, false);
+    }
+}
+));
+
 
 games = []
 
@@ -12,8 +53,24 @@ server.listen(3000, function () {
 });
 
 app.get('/', function(req,res) {
-  res.sendfile(__dirname + '/public/index.html')
+  res.sendFile(__dirname + '/public/index.html')
 });
+app.get('/home', function(req,res) {
+  res.sendFile(__dirname + '/public/home.html')
+});
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/home',
+                                      failureRedirect: '/' }));
 
 io.sockets.on('connection',function(socket){
   socket.on('newGame', function(data) {
