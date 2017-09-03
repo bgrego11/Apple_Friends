96c4f5fec11ff7861949346819f8182f11e8b7aa
bgrego11 <makhno@makhnos-MacBook-Pro.local>
2017-09-03 19:14:41 -0400

    mongo started

---
+++ gameModel.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var gameSchema = new Schema({
+  name: String,
+  host:  Schema.Types.Mixed,
+  players: [],
+  bdeck: [],
+  wdeck: [],
+  dealer: Schema.Types.Mixed,
+  rules: Schema.Types.Mixed
+ 
+});
+
+// Compile model from schema
+var Game = mongoose.model('Game', gameSchema );
+
+
+// function newDeck() {
+// Card.find({} function (err, cards) {
+//     if (err) return handleError(err);
+//     console.log(cards) // Space Ghost is a talk show host.
+//   })
+// }
+
+module.exports = gameSchema;
